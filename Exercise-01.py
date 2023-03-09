a694802ce429de225b5a90953becae920efd46f5
chanisaulkarimah <chanisaulkarimah27@gmail.com>
2023-03-10 06:31:25 +0700

    commit pertama

---
+++ Exercise-01.py
@@ -0,0 +1,9 @@
+print("Chanisa Ulkarimah")
+print("I0322026")
+
+print("=============================================================================================================================")
+
+print("     Saya tinggal di kota yang dikenal sebagai kota produksi susu terbesar di Jawa Tengah, Kota Boyolali. Jarak antara tempat   tinggalku dengan Universitas Sebelas Maret sekitar kurang lebih 25 km. Kebetulan tempat tinggalku berdekatan dengan dua         destinasi wisata yang sedang tren belakangan ini, yaitu Waduk Cengklik dan Kali Pepe Land. Kedua wisata ini ramai dikunjungi    ketika menjelang sore hari dan digunakan untuk menikmati sunset dan waktu bersama keluarga. Selain itu, ada Adi Sumarmo         International Airport yang sering digunakan untuk bersantai sembari mengobrol atau hanya sekedar melihat pesawat take off.")
+print("     Harapan saya setelah lulus dan menjadi Sarjana Teknik Industri, yaitu saya ingin bekerja di salah satu perusahaan yang     dapat membantu saya ke depannya. Selama bekerja di perusahaan, saya akan menabung dan mengumpulkan pengalaman untuk merintis    usaha sendiri. Sehingga, saya berharap agar ilmu dan pengalaman yang saya dapatkan selama berkuliah dapat saya gunakan dengan   sebaik-baiknya terutama untuk masyarakat sekitar.")
+
+print("=============================================================================================================================")
